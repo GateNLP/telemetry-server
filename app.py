72f456f383634b3dfa9c6afaa3581961754ea8b1
Ian Roberts <i.roberts@dcs.shef.ac.uk>
2023-01-12 15:02:36 +0000

    Make ProxyFix conditional on an environment variable

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 import json
+import os
 
 from datetime import datetime
 from flask import request
 from flask_api import FlaskAPI, status
-from werkzeug.middleware.proxy_fix import ProxyFix
 
 VALID_PRODUCT_NAMES = ['teamware']
 
 app = FlaskAPI(__name__)
 
-# https://flask.palletsprojects.com/en/2.2.x/deploying/proxy_fix/
-app.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
-)
+try:
+    proxies = int(os.environ.get('REVERSE_PROXY', '0'))
+    if proxies > 0:
+        from werkzeug.middleware.proxy_fix import ProxyFix
+        # https://flask.palletsprojects.com/en/2.2.x/deploying/proxy_fix/
+        app.wsgi_app = ProxyFix(
+            app.wsgi_app, x_for=proxies, x_proto=proxies, x_host=proxies, x_prefix=proxies
+        )
+except ValueError:
+    # REVERSE_PROXY environment variable not parseable as a number, assume no proxy
+    print("WARNING: invalid value for REVERSE_PROXY in environment - ignored")
+
 
 @app.post("/phone_home")
 def answer_phone_home():
